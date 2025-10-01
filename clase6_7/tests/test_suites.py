0a58738c88ebd20733eee2a2ce17afefdc36af01
Fran <fhernandez@netcomplusve.com>
2025-10-01 14:21:30 -0400

    clase7

---
+++ clase6_7/tests/test_suites.py
@@ -0,0 +1,14 @@
+import unittest
+
+from clase6_7.tests.test_bank_account import BankAccountTests2
+
+
+def bank_account_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(BankAccountTests2("test_deposit"))
+    suite.addTest(BankAccountTests2("test_withdraw"))
+    return suite
+
+if __name__ == "__main__":
+    runner = unittest.TextTestRunner()
+    runner.run(bank_account_suite())
